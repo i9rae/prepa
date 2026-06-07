1c2ab1ed6f43ab377e73fc25dfc988e407a95229
i9rae <3abd.al3ali.almalek@gmail.com>
2026-06-07 20:23:09 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>CPGE Maroc</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
 
-<h1>CPGE Maroc</h1>
-
-<p>Mon premier site est en ligne !</p>
-
-</body>
-</html>
